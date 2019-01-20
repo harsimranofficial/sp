848156c659886d145d6a3f026028321a8bfba208
harsimran <developer.erharsimran@gmail.com>
2019-01-20 09:56:10 -0800

    harsimran strings

---
+++ strings.py
@@ -0,0 +1,106 @@
+txt = "hello,a."          #1
+x= txt.capitalize()
+print (x)
+
+txt = "hello,b"          #2
+x = txt.casefold()
+print(x)
+
+x = txt.center(20)       #3
+print(x)
+
+x = txt.count("python")          #4
+
+print(x)
+
+txt = "My name is Harsimran"           #5
+
+x = txt.encode()
+
+print()
+x = txt.endswith(".")                 #6
+
+print(x)
+
+txt = "H\ta\tr\tlrts"              #7
+
+x =  txt.expandtabs(2)
+
+print(x)
+
+txt = "Hello, welcome to my world."   #8
+
+
+print(x)
+
+x = txt.index("welcome")            #9
+
+print(x)                  
+
+x = txt.isalpha()       #10
+
+print(x) 
+x = txt.lower()                  #11
+
+print(x) 
+myTuple = ("har", "sim", "ran")
+
+x = "#".join(myTuple)
+
+print(x)
+
+txt = "apple"          #12
+
+x = txt.ljust(20)
+
+print(x, "is my favorite fruit.")             #13
+
+txt = "I could eat bananas all day"
+
+x = txt.partition("bananas")
+
+print(x)
+
+txt = "Hello my FRIENDS"                   #14
+
+x = txt.lower()
+
+print(x) 
+
+txt = "hi harsimran, how are u."            #15
+
+x = txt.rfind("casa")
+
+print(x)
+
+txt = "never, ever."                        #16
+
+x = txt.rindex("casa")
+
+print(x) 
+
+txt = "welcome to my world"                 #17
+
+x = txt.split()
+
+print(x)
+          
+     
+x = txt.title()                         #18
+
+print(x)
+
+txt = "Hello my friends"                   #19
+
+x = txt.upper()
+
+print(x)
+
+xt = "50"                                  #20
+
+x = txt.zfill(10)
+
+print(x) 
+
+
+
